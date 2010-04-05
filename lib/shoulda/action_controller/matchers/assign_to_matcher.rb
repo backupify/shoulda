f766956984b8ca437ec4afac0b0d8da25ad65158
Matt Conway <wr0ngway@yahoo.com>
2010-04-05 10:56:34 -0400

    rails3 deprecations/fixes

--- lib/shoulda/action_controller/matchers/assign_to_matcher.rb
+++ lib/shoulda/action_controller/matchers/assign_to_matcher.rb
@@ -20,6 +20,7 @@ module Shoulda # :nodoc:
       end
 
       class AssignToMatcher # :nodoc:
+        include ActionDispatch::TestProcess
 
         def initialize(variable)
           @variable = variable.to_s
@@ -96,10 +97,6 @@ module Shoulda # :nodoc:
 
         def assigned_value
           assigns[@variable]
-        end
-
-        def assigns
-          @controller.response.template.assigns 
         end
 
       end
